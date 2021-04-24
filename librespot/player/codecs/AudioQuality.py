fb32cc0d99016438d8d27d2d9c9c812b8428f2c0
Restyled.io <commits@restyled.io>
2021-04-24 12:25:57 +0000

    Restyled by autopep8

--- librespot/player/codecs/AudioQuality.py
+++ librespot/player/codecs/AudioQuality.py
@@ -12,18 +12,18 @@ class AudioQuality(enum.Enum):
     @staticmethod
     def get_quality(audio_format: AudioFile.Format) -> AudioQuality:
         if audio_format == AudioFile.MP3_96 or \
-                   audio_format == AudioFile.OGG_VORBIS_96 or \
-                   audio_format == AudioFile.AAC_24_NORM:
+                audio_format == AudioFile.OGG_VORBIS_96 or \
+                audio_format == AudioFile.AAC_24_NORM:
             return AudioQuality.NORMAL
         if audio_format == AudioFile.MP3_160 or \
-                        audio_format == AudioFile.MP3_160_ENC or \
-                        audio_format == AudioFile.OGG_VORBIS_160 or \
-                        audio_format == AudioFile.AAC_24:
+                audio_format == AudioFile.MP3_160_ENC or \
+                audio_format == AudioFile.OGG_VORBIS_160 or \
+                audio_format == AudioFile.AAC_24:
             return AudioQuality.HIGH
         if audio_format == AudioFile.MP3_320 or \
-                        audio_format == AudioFile.MP3_256 or \
-                        audio_format == AudioFile.OGG_VORBIS_320 or \
-                        audio_format == AudioFile.AAC_48:
+                audio_format == AudioFile.MP3_256 or \
+                audio_format == AudioFile.OGG_VORBIS_320 or \
+                audio_format == AudioFile.AAC_48:
             return AudioQuality.VERY_HIGH
         raise RuntimeError("Unknown format: {}".format(format))
 
