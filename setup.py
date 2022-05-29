98f46faa8e6a757e60cebdcbcea93786b7b2fe33
kokarare1212 <kokarare1212@gmail.com>
2022-05-29 10:52:00 +0900

    Bypass dependency error

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 setuptools.setup(name="librespot",
-                 version="0.0.1",
+                 version="0.0.2",
                  description="Open Source Spotify Client",
                  long_description=open("README.md").read(),
                  long_description_content_type="text/markdown",
@@ -9,10 +9,7 @@ setuptools.setup(name="librespot",
                  url="https://github.com/kokarare1212/librespot-python",
                  license="Apache-2.0",
                  packages=setuptools.find_packages("."),
-                 install_requires=[
-                     "defusedxml", "protobuf", "pycryptodomex", "pyogg",
-                     "requests", "websocket-client", "zeroconf"
-                 ],
+                 install_requires=open("requirements.txt").read().splitlines(),
                  classifiers=[
                      "Development Status :: 1 - Planning",
                      "License :: OSI Approved :: Apache Software License",
