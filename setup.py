5c54873b15b7b5f07921b6cbf67a12d92440fc66
Restyled.io <commits@restyled.io>
2022-07-18 18:47:52 +0000

    Restyled by black

--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
 import setuptools
 
-setuptools.setup(name="librespot",
-                 version="0.0.6",
-                 description="Open Source Spotify Client",
-                 long_description=open("README.md").read(),
-                 long_description_content_type="text/markdown",
-                 author="kokarare1212",
-                 url="https://github.com/kokarare1212/librespot-python",
-                 license="Apache-2.0",
-                 packages=setuptools.find_packages("."),
-                 install_requires=open("requirements.txt").read().splitlines(),
-                 classifiers=[
-                     "Development Status :: 1 - Planning",
-                     "License :: OSI Approved :: Apache Software License",
-                     "Topic :: Multimedia :: Sound/Audio"
-                 ])
+setuptools.setup(
+    name="librespot",
+    version="0.0.6",
+    description="Open Source Spotify Client",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="kokarare1212",
+    url="https://github.com/kokarare1212/librespot-python",
+    license="Apache-2.0",
+    packages=setuptools.find_packages("."),
+    install_requires=open("requirements.txt").read().splitlines(),
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "License :: OSI Approved :: Apache Software License",
+        "Topic :: Multimedia :: Sound/Audio",
+    ],
+)
